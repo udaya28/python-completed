1eb5fbd1629d0ff53ea6f65a5edad6b7e8e7c2fe
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-02-28 22:13:21 +0530

    Create CGPA.py

---
+++ CGPA.py
@@ -0,0 +1,44 @@
+#💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟
+# By udaya (19CSR118) 1 st year
+# IF YOU FIND ANY MISTAKE IN CODE YOU CAN
+# REPORT ME @ INSTA:: udaya_2831 or directly
+# share and make use of it
+print("SHARE IF YOU LIKE AND MAKE USE OF IT")
+print("GRADE TO CGPA COVERTER")
+print("FOLLOW THE INSTRUCTIONS CAREFULLY")
+n=int(input("\nEnter the total of subjects : "))
+x=[]
+print("you don't need to enter full name of subject")
+print(f"\nEnter the name of the {n} subjects ")
+for i in range(n):
+    s=input(f"{i+1} subject name : ")
+    x.append(s)
+c=[]
+print("\nEnter the Credit of following subjects ")
+for j in range(n):
+    ss=int(input(f"Credit of {x[j]} : "))
+    c.append(ss)
+print("\nLetter to Grade Point\nO  is  10\t\tA+ is  9\nA  is  8\t\tB+ is  7\nB  is  6\t\tothers is 0")
+print("\nEnter grade points in number as shown above")
+g=[]
+for k in range(n):
+    gg=int(input(f"Grade of {x[k]} : "))
+    g.append(gg)
+r=[]
+for m in range(n):
+    rr=c[m]*g[m]
+    r.append(rr)
+cr=sum(c)
+ma=sum(r)
+print("\n\tYour CGPA is 👉🏻",ma/cr)
+print(" \n\n Everybody is a genius.\n But if you judge a fish by its ability to climb a tree,\n it will spend its whole life believing that it is stupid. \n\t\t\t\t\t\t\t\t\t\t\t– Albert Einstein")
+print("\n\t\t\tHAPPY CODING")
+print("\nFor enquiry see top or bottom of the code")
+print("share if you like")
+# By udaya (19CSR118) 1 st year
+# IF YOU FIND ANY MISTAKE IN CODE YOU CAN
+# REPORT ME @ INSTA: udaya_2831 or directly
+# share and make use of it
+# 💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟
+# if you have any suggestions on improving code share me
+# insta udaya_2831
